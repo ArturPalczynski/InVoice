38704ca80a1da3541a794fa106ac9a736c420049
ArturPalczynski <artur.palczynski@gmail.com>
2015-10-06 11:37:45 +0200

    First commit

--- java/main/Main.java
+++ java/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
+import inVoiceData.Address;
+import inVoiceData.Customer;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-
-import domain.Address;
-import domain.Customer;
 
 public class Main {
 
